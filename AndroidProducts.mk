696721a57c0f4dde632c07060338a0313aa7549d
Gabri <gabricarle2008@gmail.com>
2024-12-28 22:02:38 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lmodroid_z3s.mk
+    $(LOCAL_DIR)/lineage_z3s.mk
 
 COMMON_LUNCH_CHOICES := \
-    lmodroid_z3s-user \
-    lmodroid_z3s-userdebug \
-    lmodroid_z3s-eng
+    lineage_z3s-user \
+    lineage_z3s-userdebug \
+    lineage_z3s-eng
